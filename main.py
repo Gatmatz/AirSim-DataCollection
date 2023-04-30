72c18cf1d5de0aba9f19feda2683f362496cbab2
Giorgos Atmatzidis <gatmatzd@csd.auth.gr>
2023-04-30 23:10:22 +0300

    Change drone course due to new environment. New labeling.

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import os
 import cv2
 import sys
 import time
+from airsim import Vector3r
 
 # ----------------- SETTING -----------------#
-import numpy as np
 
 cameraTypeMap = {
     "depth": airsim.ImageType.DepthVis,
@@ -25,7 +25,7 @@ if cameraType not in cameraTypeMap:
 else:
     print(cameraTypeMap[cameraType])
 
-cameraName = "front_center"
+cameraName = "bottom_center"
 if cameraName not in cameraNameMap:
     sys.exit(1)
 else:
@@ -43,7 +43,50 @@ directory = "generated-data"
 if not os.path.exists(directory):
     os.makedirs(directory)
 
+
 # ----------------- FLYING -----------------#
+def captureImages(i):
+    # capture image
+    raw = client.simGetImage(cameraName, cameraTypeMap[cameraType])
+    drone_position = client.simGetVehiclePose().position
+    t = Vector3r(drone_position.x_val, drone_position.y_val, drone_position.z_val)
+    x_val = "{:.2f}".format(t.x_val)
+    y_val = "{:.2f}".format(t.y_val)
+    z_val = "{:.2f}".format(t.z_val)
+    str_position = 'x:' + x_val + ' ' + 'y: ' + y_val + ' ' + 'z:' + z_val
+    # print(str_position)
+    if raw is None:
+        print("Camera is not returning image, please check airsim for error messages")
+        sys.exit(0)
+    else:
+        png = cv2.imdecode(airsim.string_to_uint8_array(raw), cv2.IMREAD_UNCHANGED)
+        img_name = str_position
+        cv2.putText(png, img_name, textOrg, fontFace, fontScale, (0, 255, 255), thickness)
+        file_name = 'Frame ' + str(i) + '.png'
+        cv2.imwrite(os.path.join(directory, file_name), png)
+
+
+def performCycle(velocity):
+    for i in range(10):
+        # move forward
+        client.moveByVelocityAsync(velocity, 0, 0, 1).join()
+        captureImages(i)
+
+    for i in range(10, 17):
+        # move left
+        client.moveByVelocityAsync(0, velocity, 0, 1).join()
+        captureImages(i)
+
+    for i in range(17, 25):
+        # move left
+        client.moveByVelocityAsync(-velocity, 0, 0, 1).join()
+        captureImages(i)
+
+    for i in range(25, 31):
+        # move left
+        client.moveByVelocityAsync(0, -velocity, 0, 1).join()
+        captureImages(i)
+
 
 # connect to the AirSim simulator using Tornado
 client = airsim.MultirotorClient()
@@ -51,26 +94,12 @@ client.confirmConnection()
 client.enableApiControl(True)
 
 # take off
-startTime = time.time()
 client.takeoffAsync().join()
-
-# Capture Images
-for i in range(10):
-    # move forward
-    client.moveByVelocityAsync(5, 0, 0, 0.1).join()
-    endTime = time.time()
-    diff = endTime - startTime
-    # capture image
-    raw = client.simGetImage(cameraName, cameraTypeMap[cameraType])
-    if raw is None:
-        print("Camera is not returning image, please check airsim for error messages")
-        sys.exit(0)
-    else:
-        png = cv2.imdecode(airsim.string_to_uint8_array(raw), cv2.IMREAD_UNCHANGED)
-        img_name = 'Frame:'+str(i) + ' ' 'Time:' + str("{:.2f}".format(diff))
-        cv2.putText(png, img_name, textOrg, fontFace, fontScale, (0, 255, 255), thickness)
-        file_name = 'Frame ' + str(i) + '.png'
-        cv2.imwrite(os.path.join(directory, file_name), png)
+for i in range(3):
+    client.moveByVelocityAsync(0, 7, 0, 1).join()
+# Fly
+velocity = 10
+performCycle(velocity)
 
 # Land Drone
 client.landAsync().join()
